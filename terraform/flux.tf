d99160330b4581a588a3019e809fdb05d5b63b4b
George <george_agriogiannis@yahoo.com>
2026-01-26 15:47:38 +0100

    Switching to url from branch

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -3,7 +3,7 @@ resource "flux_bootstrap_git" "this" {
   depends_on = [google_container_cluster.main]
 
   embedded_manifests = true
-  branch             = "master"
+  url                = "https://github.com/Gagrio/gitops"
   path               = var.flux_target_path
 
   components_extra = [
